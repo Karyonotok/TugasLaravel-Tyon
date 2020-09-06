70d7a2dd904fccc0db9fb5909635343942e6af73
Karyonotok <mustikahikmah07@gmail.com>
2020-09-06 20:40:49 +0700

    first

---
+++ resources/views/index.blade.php
@@ -0,0 +1,19 @@
+@extends('layout.main')
+
+@section('title', 'Web latihan' )
+
+@section('container')
+
+
+<div class="container">
+      <div class="row">
+          <div class="col-10">
+              <div class="col-10">
+
+                      <h1 class="mt-2">Hello, World!</h1>
+            </div>
+        </div>
+    </div>
+</div>
+
+@endsection
