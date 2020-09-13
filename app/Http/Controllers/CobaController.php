ff8b2c66b66e21853e07eba068caa92bc4cb424a
Karyonotok <mustikahikmah07@gmail.com>
2020-09-14 00:53:40 +0700

    Tugas kedua

---
+++ app/Http/Controllers/CobaController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class CobaController extends Controller
+{
+    public function show(){
+        return "Tes controller berhasil";
+
+    }
+
+    public function insert(){
+
+
+    }
+
+    public function delete(){
+
+
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -22,3 +22,28 @@ Route::get('/about', function () {
     $nama = 'Digna Kurnia Lestari!';
     return view('about', ['nama' => $nama]);
 });
+
+// Closure
+    Route::get('/profil', function () {
+        return "Ini adalah profil siswa";
+
+    })->name('profil');
+
+// Route name
+    Route::get("/tesname", function () {
+        return route('profil');
+    });
+
+// Parameter
+    Route::get('/profil/{id}', function($id) {
+        return $id;
+
+    });
+
+// Controller
+    Route::get('/cobacontroller', "CobaController@show");
+
+
+// Resource
+    Route::resource('siswa', "SiswaController");
+
